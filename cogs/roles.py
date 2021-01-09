bb36b77521d03d1d665e8392a6755f7e1aa3f370
Ivan A. Reyes <42950201+ItsLaro@users.noreply.github.com>
2021-01-08 22:36:28 -0400

    Temporarily removed sheets and rental cogs, improved roles cog to fit current Discord Guild structure

--- cogs/roles.py
+++ cogs/roles.py
@@ -4,41 +4,68 @@ from discord.ext import commands
 class Roles(commands.Cog):
 
     '''
-    Behaviour of the bot and its sorroundings
+    Handles user verification by reacting to the roles and assignment of other roles via reactions
     '''
     def __init__(self, bot):
         self.bot = bot
 
         #Channels
-        self.BOT_LOGS_CHANNEL_ID = 746081458564890654
+        self.BOT_LOGS_CHANNEL_ID = 789520898356412477
         self.log_channel = self.bot.get_channel(self.BOT_LOGS_CHANNEL_ID)
 
         #Messages
-        self.RULES_ACCEPTANCE_MESSAGE_ID = 746080888571297853
-        self.ROLE_ASSIGNMENT_MESSAGE_ID = 769952501386313728
-
+        self.RULES_ACCEPTANCE_MESSAGE_ID = 797210701604978698
+        self.ROLE_UPE_ASSIGNMENT_MESSAGE_ID = 797208810577723402
+        self.ROLE_SOCIAL_ASSIGNMENT_MESSAGE_ID = 797213192811053147
+        
         #Roles
+        self.CODE_ROLE_NAME = "Code"
+        self.MAKE_ROLE_NAME = "Make"
+        self.INFOTECH_ROLE_NAME = "Infotech"
+        self.DESIGN_ROLE_NAME = "Design"
+        self.ADVANCE_ROLE_NAME = "Advance"
+        self.IGNITE_ROLE_NAME = "Ignite"
+        self.SPARKDEV_ROLE_NAME = "Sparkdev"
+        self.MENTOR_ROLE_NAME = "Mentor"
+        self.SHELLHACKS_ROLE_NAME = "Shellhacks"
+        self.DISCOVER_ROLE_NAME = "Discover"
 
         #Emojis
-        self.emojis = {
-            "RULES_ACCEPTANCE_EMOJI_NAME" :'\N{WHITE HEAVY CHECK MARK}',
-            "WEB_INTEREST_EMOJI" : "<:web_dev:753272557800784024>",
-            "AI_INTEREST_EMOJI" :"<:artificial_intelligence:753272557737607369>",
-            "HARDWARE_INTEREST_EMOJI" : "<:robotics:753272557867761763>",
-            "GAMING_INTEREST_EMOJI" : "<:partaaaay:682027649568342081>"
-            }
+        self.emoji_rules = '\N{WHITE HEAVY CHECK MARK}'
+        self.emojis_upe = {
+            self.CODE_ROLE_NAME : "<:code:753692446738612384>",
+            self.MAKE_ROLE_NAME : "<:make:753692447044927518>",
+            self.INFOTECH_ROLE_NAME :"<:infotech:753692446726029388>",
+            self.DESIGN_ROLE_NAME : "<:design:797279164548907029>",
+            self.ADVANCE_ROLE_NAME : "<:advance:753692446776361126>",
+            self.IGNITE_ROLE_NAME : "<:ignite:753692446801789039>",
+            self.SPARKDEV_ROLE_NAME : "<:sparkdev:753692446784880760>",
+            self.MENTOR_ROLE_NAME : "<:mentor:753692446776623284>",
+            self.SHELLHACKS_ROLE_NAME : "<:shellhacks:753692446621433927>",
+            self.DISCOVER_ROLE_NAME : "<:discover:753692446524702782>",
+        }
+        self.emojis_misc = {
+            "OPPORTUNITIES" : "",
+            "WOMENINTECH" : "",
+            "FITNESSCREW" : "",
+            "AMONGUS" : "",
+            "GACHA" : "",
+            "TAMAGOTCHI" : ""
+        }
 
     #Events
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
 
         reacting_user = payload.member
-        print("User " + reacting_user.name + " reacted with: " + payload.emoji.name)
 
+        ### RULES ACCEPTANCE
+    
+        # User added reaction to the Rules Acceptance Message in #welcome
         if payload.message_id == self.RULES_ACCEPTANCE_MESSAGE_ID:
 
-            # User reacts on the appropiate message AND with the appropiate emoji
-            if payload.emoji.name == self.emojis["RULES_ACCEPTANCE_EMOJI_NAME"]:
+            # User reacted with the appropiate emoji
+            if payload.emoji.name == self.emoji_rules:
                 # Sets the verified 'User' role
                 verified_user_role = discord.utils.get(reacting_user.guild.roles, name="User")
                 await reacting_user.add_roles(verified_user_role) 
@@ -47,16 +74,72 @@ class Roles(commands.Cog):
                 print("User " + reacting_user.name + " has read and accepted the rules!")
                 await self.log_channel.send(f'{reacting_user.mention} has read and accepted the rules!')
 
-        if payload.message_id == self.ROLE_ASSIGNMENT_MESSAGE_ID:
+        ### UPE ROLES ASSIGNMENT
 
-            if str(payload.emoji) == self.emojis["WEB_INTEREST_EMOJI"]:
+        # User added reaction to the UPE Roles Message in #roles
+        if payload.message_id == self.ROLE_UPE_ASSIGNMENT_MESSAGE_ID:
+            
+            desired_user_role = None
+
+            #Reaction is for Code:
+            if str(payload.emoji) == self.emojis_upe[self.CODE_ROLE_NAME]:
                 # Sets the verified 'Interested' role (placeholder)
-                Placeholder_user_role = discord.utils.get(reacting_user.guild.roles, name="Interested")
-                await reacting_user.add_roles(Placeholder_user_role) 
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.CODE_ROLE_NAME)
+            
+            #Reaction is for Make:
+            elif str(payload.emoji) == self.emojis_upe[self.MAKE_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.MAKE_ROLE_NAME)
+            
+            #Reaction is for Infotech:
+            elif str(payload.emoji) == self.emojis_upe[self.INFOTECH_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.INFOTECH_ROLE_NAME)
 
+            #Reaction is for Design:
+            elif str(payload.emoji) == self.emojis_upe[self.DESIGN_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.DESIGN_ROLE_NAME)
+
+            #Reaction is for Advance:
+            elif str(payload.emoji) == self.emojis_upe[self.ADVANCE_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.ADVANCE_ROLE_NAME)
+
+            #Reaction is for Ignite:
+            elif str(payload.emoji) == self.emojis_upe[self.IGNITE_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.IGNITE_ROLE_NAME)
+
+            #Reaction is for Sparkdev:
+            elif str(payload.emoji) == self.emojis_upe[self.SPARKDEV_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.SPARKDEV_ROLE_NAME)
+
+            #Reaction is for Mentor:
+            elif str(payload.emoji) == self.emojis_upe[self.MENTOR_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.MENTOR_ROLE_NAME)
+
+            #Reaction is for ShellHacks:
+            elif str(payload.emoji) == self.emojis_upe[self.SHELLHACKS_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.SHELLHACKS_ROLE_NAME)
+
+            #Reaction is for Discover:
+            elif str(payload.emoji) == self.emojis_upe[self.CODE_ROLE_NAME]:
+                # Sets the verified 'Interested' role (placeholder)
+                desired_user_role = discord.utils.get(reacting_user.guild.roles, name=self.DISCOVER_ROLE_NAME)
+
+            #Invalid reaction (unreachable condition)
+            else:
+                pass
+            
+            if desired_user_role is not None:
+                await reacting_user.add_roles(desired_user_role) 
                 #Prints to console and notifies bot-log channel
-                print("User " + reacting_user.name + " is interested in something!")
-                await self.log_channel.send(f'{reacting_user.mention} is interested in something! <a:pumpIt:439920250302103562>')
+                print("User " + reacting_user.name + f" is interested in {desired_user_role}!")
+                await self.log_channel.send(f'{self.emojis_upe[str(desired_user_role)]} {reacting_user.mention} is interested in {desired_user_role}!')
 
 def setup(bot):
     bot.add_cog(Roles(bot)) 
--- cogs/utility.py
+++ cogs/utility.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Utilities(commands.Cog):
 
     '''
-    Administrative tools to scan integrity of the bot
+    Administrative tools to scan integrity of the bot or manage channels
     '''
 
     def __init__(self, bot):
@@ -20,8 +20,19 @@ class Utilities(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'--Pong! That took {round(self.bot.latency * 1000)}ms')
 
-    #@commands.command()
-    #async def clear(self, ctx, amount):
+    # Deletes as many messages as passed in parameter
+    @commands.command()
+    async def purge(self, ctx, num_messages):
+        # roles = ctx.author.roles
+        # admin = ctx.guild.get_role(admin_id)
+
+        # if admin not in roles:
+        #     await ctx.send(
+        #         f'{ctx.author.mention} this command is only usable by organizers.')
+        # else:
+        await ctx.channel.purge(limit=int(num_messages) + 1)
+        #@commands.command()
+        #async def clear(self, ctx, amount):
 
 def setup(bot):
     bot.add_cog(Utilities(bot)) 
