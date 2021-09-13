2839e0d1133f6527750035c6b87354c8b99bf192
ItsLaro <ivan.reor@gmail.com>
2021-09-13 16:39:43 -0400

    Updated SparkDev teams emails and refactored teamup command

--- cogs/sparkdev.py
+++ cogs/sparkdev.py
@@ -20,22 +20,20 @@ class SparkDev(commands.Cog):
         
         #Emails
         self.ai_emails = create_list_from_file("ai.txt")
+        self.bsi_emails = create_list_from_file("bsi.txt")
         self.cybersecurity_emails = create_list_from_file("cybersecurity.txt")
         self.dig_emails = create_list_from_file("dig.txt")
+        self.embeddedsys_emails = create_list_from_file("embeddedsys.txt")
         self.gamedev_emails = create_list_from_file("gamedev.txt")
         self.iot_emails = create_list_from_file("iot.txt")
         self.it_emails = create_list_from_file("it.txt")
-        self.mobiledev_emails = create_list_from_file("mobiledev.txt")
         self.robotics_emails = create_list_from_file("robotics.txt")
-        self.underline_emails = create_list_from_file("underline.txt")
         self.vr_emails = create_list_from_file("vr.txt")
-        self.webdev_emails = create_list_from_file("webdev.txt")
 
     @commands.Cog.listener()
     async def on_message(self, payload):
         if payload.channel == self.checkin_channel:
             await payload.delete()
-        #await self.bot.process_commands(payload)
 
     @commands.command()
     async def teamup(self, ctx, email=None):
@@ -45,14 +43,16 @@ class SparkDev(commands.Cog):
             await ctx.author.dm_channel.send('You must input the email you used in your application after the command.\n( Ex: `?teamup email@example.com` ).')
             return
         else:
-            lower_email = email.lower()
+            lower_email = email.strip().lower()
 
         if ctx.channel.id == self.CHECKIN_CHANNEL_ID:
 
             sparkdev_role = discord.utils.get(ctx.guild.roles, id=523307753498738692)
             ai_role = discord.utils.get(ctx.guild.roles, id=746939552999932004)
+            bsi_role = discord.utils.get(ctx.guild.roles, id=879094683979092009)
             cybersecurity_role = discord.utils.get(ctx.guild.roles, id=804734475010965534)
             dig_role = discord.utils.get(ctx.guild.roles, id=746944338063392789)
+            embeddedsys_role = discord.utils.get(ctx.guild.roles, id=879092820588257311)
             gamedev_role = discord.utils.get(ctx.guild.roles, id=746939550861099079)
             iot_role = discord.utils.get(ctx.guild.roles, id=804724602747879434)
             it_role = discord.utils.get(ctx.guild.roles, id=746944336784261291)
@@ -62,7 +62,7 @@ class SparkDev(commands.Cog):
             vr_role = discord.utils.get(ctx.guild.roles, id=747127660873908354)
             webdev_role = discord.utils.get(ctx.guild.roles, id=746939549644619807)
 
-            roles = [ai_role, cybersecurity_role, dig_role, gamedev_role, iot_role, it_role, mobiledev_role, robotics_role, underline_role, vr_role, webdev_role]
+            roles = [ai_role, bsi_role, cybersecurity_role, dig_role, embeddedsys_role, gamedev_role, iot_role, it_role, mobiledev_role, robotics_role, underline_role, vr_role, webdev_role]
 
             # check if user has a SparkDev role already
             for role in ctx.author.roles:
@@ -73,85 +73,32 @@ class SparkDev(commands.Cog):
 
             if lower_email in self.ai_emails:
                 team_role = ai_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
+            elif lower_email in self.bsi_emails:
+                team_role = bsi_role
             elif lower_email in self.cybersecurity_emails:
                 team_role = cybersecurity_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
             elif lower_email in self.dig_emails:
                 team_role = dig_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
+            elif lower_email in self.embeddedsys_emails:
+                team_role = embeddedsys_role
             elif lower_email in self.gamedev_emails:
                 team_role = gamedev_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
             elif lower_email in self.iot_emails:
                 team_role = iot_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
             elif lower_email in self.it_emails:
                 team_role = it_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
-            elif lower_email in self.mobiledev_emails:
-                team_role = mobiledev_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
             elif lower_email in self.robotics_emails:
                 team_role = robotics_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
-            elif lower_email in self.underline_emails:
-                team_role = underline_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
             elif lower_email in self.vr_emails:
                 team_role = vr_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
-            elif lower_email in self.webdev_emails:
-                team_role = webdev_role
-                await ctx.author.add_roles(team_role)
-                await ctx.author.add_roles(sparkdev_role)
-                await ctx.author.create_dm()
-                await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
-                return
             else:
                 await ctx.author.create_dm()
                 await ctx.author.dm_channel.send('Email not found...\nPlease make sure you are inputting the email you used in your application. \n( Ex: `?teamup email@example.com` ).')
                 return
+            await ctx.author.add_roles(team_role)
+            await ctx.author.add_roles(sparkdev_role)
+            await ctx.author.create_dm()
+            await ctx.author.dm_channel.send(f'{ctx.author.mention} you now have **{str(team_role)}** role!')
 
 def create_list_from_file(file_name):
     with open('db/sparkdev/' + file_name) as f:
