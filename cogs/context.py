716b973bbce6f3adf4d42629e20cca756538245a
ItsLaro <ivan.reor@gmail.com>
2022-01-18 22:57:49 -0500

    Refactor to the use of regex

--- cogs/context.py
+++ cogs/context.py
@@ -57,6 +57,53 @@ class BotContext(commands.Cog):
         for role_id in self.UNPROTECTED_ROLES_IDS:
             self.UNPROTECTED_ROLES.append(self.upe_guild.get_role(role_id))
 
+        self.url_pattern = r'''(?xi)
+            \b
+            (							# Capture 1: entire matched URL
+            (?:
+                https?:				# URL protocol and colon
+                (?:
+                /{1,3}						# 1-3 slashes
+                |								#   or
+                [a-z0-9%]						# Single letter or digit or '%'
+                                                # (Trying not to match e.g. "URI::Escape")
+                )
+                |							#   or
+                                            # looks like domain name followed by a slash:
+                [a-z0-9.\-]+[.]
+                (?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj| Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)
+                /
+            )
+            (?:							# One or more:
+                [^\s()<>{}\[\]]+						# Run of non-space, non-()<>{}[]
+                |								#   or
+                \([^\s()]*?\([^\s()]+\)[^\s()]*?\)  # balanced parens, one level deep: (…(…)…)
+                |
+                \([^\s]+?\)							# balanced parens, non-recursive: (…)
+            )+
+            (?:							# End with:
+                \([^\s()]*?\([^\s()]+\)[^\s()]*?\)  # balanced parens, one level deep: (…(…)…)
+                |
+                \([^\s]+?\)							# balanced parens, non-recursive: (…)
+                |									#   or
+                [^\s`!()\[\]{};:'".,<>?«»“”‘’]		# not a space or one of these punct chars
+            )
+            |					# OR, the following to match naked domains:
+            (?:
+                (?<!@)			# not preceded by a @, avoid matching foo@_gmail.com_
+                [a-z0-9]+
+                (?:[.\-][a-z0-9]+)*
+                [.]
+                (?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tech|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj| Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)
+                \b
+                /?
+                (?!@)			# not succeeded by a @, avoid matching "foo.na" in "foo.na@example.com"
+            )
+        )'''
+        self.gifs_pattern = r'^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$|\W*(tenor.com)\W*|\W*(gfycat.com)\W*|\W*(imgur.com)\W*|\W*(giphy.com)\W*'
+        self.mentions_nitro_pattern = r'\W*(discord)\W*|\W*(nitro)\W*'
+        self.real_nitro_pattern = r'\W*\b(discord.com)|\W*\b(discord.gg)\W*|\W*\b(discordapp.com)\W*|\W*\b(https:\/\/discord.com)\W*|\W*\b(https:\/\/discord.gg)\W*|\W*\b(https:\/\/discordapp.com)\W*'
+
     #Commands
     @commands.command()
     async def speak(self, ctx, *args):
@@ -129,67 +176,23 @@ class BotContext(commands.Cog):
                 # maybe you dont have permission to do that
                 # we dont mind, so we can just ignore them
                 pass 
-        
-        url_pattern = r'''(?xi)
-            \b
-            (							# Capture 1: entire matched URL
-            (?:
-                https?:				# URL protocol and colon
-                (?:
-                /{1,3}						# 1-3 slashes
-                |								#   or
-                [a-z0-9%]						# Single letter or digit or '%'
-                                                # (Trying not to match e.g. "URI::Escape")
-                )
-                |							#   or
-                                            # looks like domain name followed by a slash:
-                [a-z0-9.\-]+[.]
-                (?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj| Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)
-                /
-            )
-            (?:							# One or more:
-                [^\s()<>{}\[\]]+						# Run of non-space, non-()<>{}[]
-                |								#   or
-                \([^\s()]*?\([^\s()]+\)[^\s()]*?\)  # balanced parens, one level deep: (…(…)…)
-                |
-                \([^\s]+?\)							# balanced parens, non-recursive: (…)
-            )+
-            (?:							# End with:
-                \([^\s()]*?\([^\s()]+\)[^\s()]*?\)  # balanced parens, one level deep: (…(…)…)
-                |
-                \([^\s]+?\)							# balanced parens, non-recursive: (…)
-                |									#   or
-                [^\s`!()\[\]{};:'".,<>?«»“”‘’]		# not a space or one of these punct chars
-            )
-            |					# OR, the following to match naked domains:
-            (?:
-                (?<!@)			# not preceded by a @, avoid matching foo@_gmail.com_
-                [a-z0-9]+
-                (?:[.\-][a-z0-9]+)*
-                [.]
-                (?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tech|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj| Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)
-                \b
-                /?
-                (?!@)			# not succeeded by a @, avoid matching "foo.na" in "foo.na@example.com"
-            )
-        )'''
-        gifs_pattern = r'^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$|\W*(tenor.com)\W*|\W*(gfycat.com)\W*|\W*(imgur.com)\W*|\W*(giphy.com)\W*'
-        sus_nitro_pattern = r'\W*(discord)\W*|\W*(nitro)\W*'
-        real_nitro_pattern = r'\W*(discord.com)\W*|\W*(discord.gg)\W*|\W*(discordapp.com)\W*'
 
-        matching_url = re.findall(pattern=url_pattern, string=content)
-        matching_gifs = re.findall(pattern=gifs_pattern, string=content)
-        matching_sus_nitro = re.findall(pattern=sus_nitro_pattern, string=content) 
-        matching_real_nitro = re.findall(pattern=real_nitro_pattern, string=content) 
 
-        if matching_url:
+        matching_url = re.findall(pattern=self.url_pattern, string=content)
+        matching_gifs = re.findall(pattern=self.gifs_pattern, string=content)
+        matching_mentions_nitro = re.findall(pattern=self.mentions_nitro_pattern, string=content) 
+        matching_real_nitro = re.findall(pattern=self.real_nitro_pattern, string=content) 
 
-            if matching_real_nitro:
-                pass # Real Discord link
+        if matching_mentions_nitro:
+            print("Discord/Nitro talk")
+            if matching_url:
 
-            elif matching_sus_nitro:
+                if matching_real_nitro:
+                    print("Real Discord URL")
+                    pass # Real Discord link
 
-                if (matching_gifs):
+                elif (matching_gifs):
+                    print("Included Media")
                     pass # Kinda sus, but just media
 
                 else: 
@@ -217,9 +220,6 @@ class BotContext(commands.Cog):
                         await author.ban(reason = f"Automated Ban: Potential Nitro Scam\nMessage:\n {message.content}")
                     await self.log_channel.send(f"<@&{self.MODERATOR_ROLE_ID}>")
                     await self.log_channel.send(embed=embed_response)
-
-            else:
-                pass # Random URL
 
 def setup(bot):
     bot.add_cog(BotContext(bot)) 
