decfe11980994c51bef5867d404b0b27386f5ce3
Gino Rey <gino.rey@outlook.com>
2023-03-14 14:38:33 -0400

    added embed messages, added checks for discord name

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -1,7 +1,7 @@
 import discord
-import config
 from discord import app_commands
 from discord.ext import tasks, commands
+import config
 import asyncio
 import datetime
 from datetime import date
@@ -15,6 +15,8 @@ AIRTABLE_TABLE_ID = os.environ['UPE_AIRTABLE_TABLE_ID']
 BIRTHDAY_FIELD_ID = 'fldoUSAvFtdsI51FP'
 DISCORD_NAME_FIELD_ID = 'fldoUSAvFtdsI51FP'
 NAME_FIELD_ID = 'fld3mtI8hayU9UPHD'
+YELLOW_COLOR = 0xFFBF00  
+RED_COLOR = 0xD2222D
 
 BOT_KEY = os.environ['BOT_KEY']
 
@@ -26,7 +28,10 @@ class Birthday(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.MARKETING_CHANNEL_ID = 1065042161265021014 # change in prod
+        self.MEMBERSHIP_CHANNEL_ID = 1061194622472298559 if config.isProd else 1065042160426176632 
+        self.BOT_LOG_CHANNEL_ID = 626541886533795850 if config.isProd else 1065042159679578154
+        self.SPOTLIGHT_CHANNEL_ID = 1019725350701379685
+        self.GUILD = bot.get_guild(245393533391863808)
         self.check_birthday.start()
     
     def unload(self):
@@ -68,26 +73,48 @@ class Birthday(commands.Cog):
         '''
         now = datetime.datetime.now()
         current_date = now.strftime("%m-%d")
+        date_formatted = now.strftime('%B %d') + ('st' if now.day in [1, 21, 31] else 'nd' if now.day in [2, 22] else 'rd' if now.day in [3, 23] else 'th')
         records = self.get_airtable_data()
-        channel = self.bot.get_channel(self.MARKETING_CHANNEL_ID)
+        
         if records is not None:
-            # send message to channel
-            message = "Good morning <@&1065042154369585270>! <a:sunrise:1084911488151597116> \n\nThese are the members celebrating birthday's today: "
+            channel = self.bot.get_channel(self.MEMBERSHIP_CHANNEL_ID)
+            response_title = f"<a:blobcake:1063101478321000558> Today's Birthdays: {date_formatted}"
+            response_description = "List of INIT Members that are celebrating their birthday's today:"
+            embed_color = YELLOW_COLOR
+            embed_response = discord.Embed(title=response_title, description=response_description, color=embed_color)
+            birthday_count = 0
+
             for record in records:
-                if 'Birthday' not in record['fields']:
+                if 'Birthday' not in record['fields'] or 'Discord User Name' not in record['fields']:
                     continue
-                
+                    
                 birthday = record['fields']['Birthday']
                 if birthday[5:10] == current_date:
                     discord_user_name = record['fields']['Discord User Name']
+                    discord_user = discord.utils.get(self.bot.guilds, name=discord_user_name)
                     first_name = record['fields']['First Name']
                     last_name = record['fields']['Last Name']
-                    message += f' {first_name} {last_name} ({discord_user_name})'
+                    embed_response.add_field(name="Name", value=f"{first_name} {last_name} ({discord_user.mention if discord_user is not None else discord_user_name})", inline=False)
+                    birthday_count += 1
+                    
+            embed_response.set_footer(text=f'\n\nWrite them something special in #spotlight <:partying_face:1085041886139523104>')
             
-            message += '\n\nWrite them something special! <:partying_face:1084911656775200899>'
-            await channel.send(message)
         else:
-            return
+            channel = self.bot.get_channel(self.BOT_CHANNEL_ID)
+            response_title = f"<a:utilfailure:809713365088993291> Failed to load birthdays"
+            response_description = "Failed to load membership records"
+            embed_color = RED_COLOR
+            embed_response = discord.Embed(title=response_title, description=response_description, color=embed_color)
+        
+        if birthday_count == 0:
+            response_title = f"<a:blobcake:1063101478321000558> Today's Birthdays: {date_formatted}"
+            response_description = "None of our members are celebrating their birthdays today."
+            embed_color = YELLOW_COLOR
+            embed_response = discord.Embed(title=response_title, description=response_description, color=embed_color)
+            await channel.send(embed=embed_response)
+
+        await channel.send(embed=embed_response)
+
 
 
 async def setup(bot):
