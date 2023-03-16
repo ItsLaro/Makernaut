547ed845c55ae0d058a9c5991af08ce023179a3d
Gino Rey <gino.rey@outlook.com>
2023-03-16 14:07:12 -0400

    initial commit

---
+++ cogs/notion_tracker.py
@@ -0,0 +1,13 @@
+
+
+class DiscordTaskRunner:
+    def __init__(self, bot):
+        self.bot = bot
+
+class NotionTaskResolver:
+    def __init__(self, bot):
+        self.bot = bot
+
+class InitTaskRunner:
+    def __init__(self, bot):
+        self.bot = bot
