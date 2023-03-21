b024e3643042f3d9cd9fe3da2f5f58759105edd5
Svilla27 <svill069@fiu.edu>
2023-03-20 21:37:03 -0500

    Moved credential check and regeneration code into the function. It is possible sending emails with no errorsC:/Users/stevo/AppData/Local/Programs/Python/Python311/python.exe c:/Users/stevo/Desktop/INIT/Makernaut/helpers/mail/email_sender.py.

--- helpers/mail/email_sender.py
+++ helpers/mail/email_sender.py
@@ -16,30 +16,32 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
-# setup
-creds = None
-# The file token.json stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        print("These are both None") #TODO: Fix token/Oauth
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'token.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
-
+to_email = 'stevenvilla70@gmail.com'
 
 def gmail_send_verification_code(to_email):
+
+    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+    # setup
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            print("These are both None") #TODO: Fix token/Oauth
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
     # Trying to send email
     try:
         # API Init
