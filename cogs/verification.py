ab583f72c268fa73120588f3867eb7dd0045ea4f
Ivan A. Reyes <42950201+ItsLaro@users.noreply.github.com>
2023-03-23 00:20:43 +0000

    Finished Discord UI for verification

--- cogs/verification.py
+++ cogs/verification.py
@@ -1,7 +1,8 @@
+import config
 import discord
 from discord.ext import commands
 from discord import app_commands
-from discord.ui import TextInput, View, Button
+from discord.ui import TextInput, View, Button, Modal
 from email_validator import validate_email, EmailNotValidError
 from helpers.airtable import get_all_records, get_record_by_email
 from helpers.mail.email_sender import gmail_send_verification_code 
@@ -12,28 +13,50 @@ AIRTABLE_UPE_MEMBERSHIP_TABLE_ID='tbluUiP1zIUtP2uwS'
 AIRTABLE_UPE_AA_BASE_KEY='appmBfrXhvebmMnbq'
 AIRTABLE_UPE_AA_MEMBERSHIP_TABLE_ID='tblGjYHulggH2gGPJ'
 
-class CodeSubmissionView(View):
-    code : str = None
+YELLOW_COLOR = 0xFFBF00  
+             
+class InitiateControls (View):
+    @discord.ui.button(label='Verify', style=discord.ButtonStyle.primary)
+    async def initiate(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_modal(VerificationCodeSubmitModal()) 
 
-    def __init__(self, timeout=900):
-        super().__init__(timeout=timeout)
-
-    @discord.ui.textInput(
-        label="Verification Code", 
-        style=discord.TextStyle.short, 
-        label="Email Verification Code",
+class VerificationCodeSubmitModal(Modal, title='Verify your Email'):
+    name = TextInput(
+        style=discord.TextStyle.short,
+        label="Verification Code",
         required=True,
         placeholder="ABCD1234",
         min_length=8,
-        max_lenght=12,
+        max_length=10,
     )
-    async def input_code(self, interaction: discord.Interaction, textInput: discord.ui.TextInput):
-        #TODO: Implement
 
-    @discord.ui.button(label="Submit", style=discord.ButtonStyle.primary)
-    async def submit(self, interaction: discord.Interaction, button: discord.ui.Button):
-        #TODO: Implement
-                            
+    async def on_submit(self, interaction: discord.Interaction):
+
+        # Compare input with server token
+        if self.name.value == 'AAAAAAAA':
+            # Erase server token
+            # Store Discord ID of user
+            title = 'Verified!'
+            description = 'Your INIT Membership is now confirmed on Discord'
+            color = discord.Color.green()
+            embed_response = discord.Embed(title=title,
+                        description=description,
+                        color=discord.Color.green(),
+            )
+            response = await interaction.response.send_message(embed=embed_response, ephemeral=True)
+        else:
+            raise Exception('Code does not match...')
+
+    async def on_error(self, interaction: discord.Interaction, error : Exception):
+        title = 'Verification Failed'
+        description = 'The code did not match. Please try again.'
+        color = discord.Color.red()
+        embed_response = discord.Embed(title=title,
+                    description=description,
+                    color=discord.Color.red(),
+        )
+        # response = await interaction.response.send_message(embed=embed_response, ephemeral=True)        
+        return error
 
 class Verification(commands.GroupCog, name="verify"):
 
@@ -44,6 +67,24 @@ class Verification(commands.GroupCog, name="verify"):
     def __init__(self, bot):
         self.bot = bot
         self.MODERATOR_ROLE_ID = 399551100799418370  
+
+    async def cog_load(self):
+        ALUMNI_VERIFY_CHANNEL_ID = 1087566994682949672 if config.isProd else 1087566806820077659
+        alumni_verification_channel = self.bot.get_channel(ALUMNI_VERIFY_CHANNEL_ID)
+
+        # We clean channel for stale messages and resend a new one
+        async for message in alumni_verification_channel.history():
+            if message.author.id == self.bot.user.id:
+                await message.delete()
+
+        # Send a new Guild start message
+        embed_title = "Verify your INIT AA Membership!"
+        embed_description = "Blah blah blah"
+        embed = discord.Embed(title=embed_title, description=embed_description, color=discord.Colour.blurple())
+
+        controls = InitiateControls(timeout=None)
+
+        self.bot_intro_message = await alumni_verification_channel.send(embed=embed, view=controls)
 
     #Commands
     @app_commands.command(name="alumni", description="Verify your membership of the INIT AA (Alumni Association)")
@@ -68,11 +109,12 @@ class Verification(commands.GroupCog, name="verify"):
                 await interaction.response.send_message(embed=embed_response, ephemeral=True)
             else:
                 #TODO: Verify that this works.
-                embed_response = discord.Embed(title="Check your inbox to verify!", description="I've sent a code to the email address you provided. Please enter it below", color=discord.Color.green())
                 verification_token = gmail_send_verification_code(email)
                 #TODO: Store verification_token in new Airtable column for the record.
-                code_submission_view = CodeSubmissionView()
-                await interaction.response.send_message(embed=embed_response, view=code_submission_view, ephemeral=True)
+
+                embed_response = discord.Embed(title="Check your inbox to verify!", description="I've sent a code to the email address you provided. Please enter it below", color=discord.Color.green())
+                modal = VerificationCodeSubmitModal()
+                await interaction.response.send_message(embed=embed_response, view=modal, ephemeral=True)
 
             # If not found:
                 # error out
