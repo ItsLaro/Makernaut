d0f1b6f2c551502405c36d7246b7d1bba2dc1b0d
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2023-03-29 23:12:50 -0400

    created add day and fetch all functions

--- cogs/database.py
+++ cogs/database.py
@@ -9,8 +9,28 @@ class Database:
         uri = "" # TODO: Figure out URI
         databaseName = ""
         self.client = MongoClient(uri)  # MongoDB Client
-        self.db = self.client[databaseName]
+        self.db = self.client[databaseName]        
 
-    def add_day_to_database(self, channelID):
-        raw_data = None  # Calls function from analytics.py to get the data
-        
+    def getGuilds(self):
+        pass
+
+    def add_day_to_database(self):
+        categoryNames= ['Guilds', 'Parties']
+        excludeChannelName = 'townhall'
+        guilds = [] # all channels within both guilds and parties category
+
+        for guild in guilds:
+            dailyAnalytics = None # document to be inserted
+            collection = self.db[guild]
+            collection.insert_one(dailyAnalytics) 
+
+    # method designed to push into the airtable interface
+    def fetch_all(self):
+        # using the db.list_collection_names() instead of channel IDs because this is a purely fetch from database function
+        collectionNames= self.db.list_collection_names()
+
+        for guild in collectionNames:
+            collection = self.db[guild]
+            documents = collection.find({})
+            for document in documents:
+                pass # TODO: add a place to store all fetched docs
