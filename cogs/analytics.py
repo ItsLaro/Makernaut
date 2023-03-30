022b3b823f5af0cf0e0977a6dccc52d96d19e25a
Gino Rey <gino.rey@outlook.com>
2023-03-29 23:09:53 -0400

    reverted back manually, im dumb

--- cogs/analytics.py
+++ cogs/analytics.py
@@ -11,7 +11,6 @@ AIRTABLE_TABLE_ID = os.getenv('AIRTABLE_TABLE_ID')
 class Analytics(commands.cog):
     '''
     Controls all analytics of guilds and parties
-
     TODO: 1. Upload all data logs to mongodb
           2. Upload analytics collected to airtable frontend
           3. Complete task loops to have it run at 3am everyday
@@ -26,85 +25,64 @@ class Analytics(commands.cog):
     def cog_unload(self):
         self.collect_analytics_loop.cancel()
 
-    #async def get_guilds_and_parties_channels_ids(self, server):
-    #    '''
-    #    Collects all guild and party channel ids
-    #    '''
-    #    category_names = ['Guilds', 'Parties']
-    #    exclude_channel_name = 'townhall'
-    #    channels = self.get_channels_under_category(server, category_names, exclude_channel_name)
-    #    return channels # returns list of channel objects
-#
-    #async def get_daily_messages(self, channel_ids):
-    #    '''
-    #    Collects all messages from the previous 24hrs, returns dictionary
-    #    daily_guilds_activity = [{channel_id: [messages objects]}]
-    #    '''
-#
-    #    now = datetime.datetime.utcnow('US/Eastern')
-    #    one_day_ago = now - datetime.datetime.timedelta(days=1)
-    #    daily_guilds_activity = [] # list of guild/parties message history 
-    #    for channel_id in channel_ids:
-    #        channel_message_history = {} # ex. {channel_id: [message_history]}
-    #        channel = self.bot.get_channel(channel_id) # retrieve channel object
-    #        if channel is None:
-    #            print(f'Channel with ID {channel_id} not found.')
-    #            continue
-#
-    #        message_history = [] # previous 24 hour message history 
-    #        for message in channel.history(limit=None, after=one_day_ago):
-    #            if message.created_at >= one_day_ago:
-    #                message_history.append(message)
-    #            else:
-    #                break
-    #            
-    #        # add 24 hour message history to channel
-    #        channel_message_history[channel_id] = message_history
-    #        # add channel history to list of all channels
-    #        daily_guilds_activity.append(channel_message_history)
-#
-    #    # returns list of dictionaries
-    #    return daily_guilds_activity 
-    #
-    #async def get_unique_visiters(self, daily_guilds_activity):
-    #    '''
-    #    Collects daily unique visiters from messages
-    #    Will return dictionary with the following format
-    #    {channel_id: [list of unique_user objects]}
-    #    '''
-#
-    #    unique_users = {}
-    #    for channel_id, messages in daily_guilds_activity.items():
-    #        unique_users = set()
-#
-    #        for message in messages:
-    #            unique_users.add(message.author.id)
-    #    
-    #        unique_users['channel_id'] = channel_id
-    #        unique_users['unique_users'] = list(unique_users)
-    #    
-    #    return unique_users
-    
-    async def get_daily_unique_visitors(self, server):
+    async def get_guilds_and_parties_channels_ids(self, server):
+        '''
+        Collects all guild and party channel ids
+        '''
         category_names = ['Guilds', 'Parties']
         exclude_channel_name = 'townhall'
         channels = self.get_channels_under_category(server, category_names, exclude_channel_name)
+        return channels # returns list of channel objects
 
-        now = datetime.datetime.utcnow()
-        one_day_ago = now - datetime.timedelta(days=1)
-        daily_unique_visitors = {}
+    async def get_daily_messages(self, channel_ids):
+        '''
+        Collects all messages from the previous 24hrs, returns dictionary
+        daily_guilds_activity = [{channel_id: [messages objects]}]
+        '''
 
-        for channel in channels:
-            channel_id = channel.id
+        now = datetime.datetime.utcnow('US/Eastern')
+        one_day_ago = now - datetime.datetime.timedelta(days=1)
+        daily_guilds_activity = [] # list of guild/parties message history 
+        for channel_id in channel_ids:
+            channel_message_history = {} # ex. {channel_id: [message_history]}
+            channel = self.bot.get_channel(channel_id) # retrieve channel object
+            if channel is None:
+                print(f'Channel with ID {channel_id} not found.')
+                continue
 
-        unique_users = set()
-        async for message in channel.history(limit=None, after=one_day_ago):
-            if message.created_at >= one_day_ago:
+            message_history = [] # previous 24 hour message history 
+            for message in channel.history(limit=None, after=one_day_ago):
+                if message.created_at >= one_day_ago:
+                    message_history.append(message)
+                else:
+                    break
+                
+            # add 24 hour message history to channel
+            channel_message_history[channel_id] = message_history
+            # add channel history to list of all channels
+            daily_guilds_activity.append(channel_message_history)
+
+        # returns list of dictionaries
+        return daily_guilds_activity 
+    
+    async def get_unique_visiters(self, daily_guilds_activity):
+        '''
+        Collects daily unique visiters from messages
+        Will return dictionary with the following format
+        {channel_id: [list of unique_user objects]}
+        '''
+
+        unique_users = {}
+        for channel_id, messages in daily_guilds_activity.items():
+            unique_users = set()
+
+            for message in messages:
                 unique_users.add(message.author.id)
-
-        daily_unique_visitors[channel_id] = list(unique_users)
-        print(daily_unique_visitors)
-        return daily_unique_visitors
+        
+            unique_users['channel_id'] = channel_id
+            unique_users['unique_users'] = list(unique_users)
+        
+        return unique_users
 
     async def check_status(self):
         '''
@@ -117,9 +95,15 @@ class Analytics(commands.cog):
         '''
         TODO: Task loop needs to be redone at the end
         '''
-    @tasks.loop(minutes=1) # change to datetime
-    async def collect_analytics_loop(self):
-        '''
-        Conducts all processes
-        '''
-        self.get_daily_unique_visiters()
+    #@tasks.loop(minutes=TIME_TO_RUN) # change to datetime
+    #async def collect_analytics_loop(self):
+    #    '''
+    #    Loops at 3am, conducts all processes
+    #    '''
+    #    # Retrieve new parties
+    #    guild_and_party_ids = self.get_guilds_and_parties_channels_ids(server=self.GUILD_ID) 
+    #    # Collect Activity
+    #    guild_and_party_message_history = await self.collect_activity(channel_ids=guild_and_party_ids)
+    #    # Calculate Analytics
+    #    await self.collect_daily_unique_visiters(messages=guild_and_party_message_history)
+    #    await self.check_status()
